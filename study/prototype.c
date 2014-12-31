327790a113160571bde766c96452a05cf20d7977
Yusuke Takahashi <t13548yt@sfc.keio.ac.jp>
2014-12-31 19:16:54 +0900

    ファイルから文字読み込み部分まで完成
    現状、ファイルが1023文字などの時whileが一度余計に回る。が、よりよい対応策は思いつかないので保留。

--- study/prototype.c
+++ study/prototype.c
@@ -4,19 +4,50 @@
 #include "getopt_long/my_getopt.h"
 #include "prototype.h"
 
+#define DBG(...) (printf("%s %u @%s(): ",__FILE__,__LINE__,__func__), printf(__VA_ARGS__)), puts("")
+#define BUF_LEN 1024
+
 int main(int argc, char *argv[])
 {
-	FILE *fp;
+	FILE *fp = NULL;
 	char buffer[BUF_LEN];
+	int i;
 
-	fp = sourceLoad(NULL, buffer, BUF_LEN, argc, argv);
+	source_load(&fp, buffer, sizeof(buffer), argc, argv);
+
+	while (1)
+	{
+		for (i=0; buffer[i] != '\0'; i++)
+		{
+			// ソースファイルを一文字ずつ表示できることを確認
+			printf("buffer[%d]:%c\n", i, buffer[i]);
+			// codeRun(buffer[i]);
+		}
+
+		// fpが終端に達していなければ追加で読み込み
+		if (!feof(fp))
+		{
+			source_load(&fp, buffer, sizeof(buffer), argc, argv);
+		}
+		// 終端に達していたらファイルの見込みを終了
+		else
+		{
+			if (fclose(fp) == EOF)
+			{
+				my_strerror(2, argv[0], argv[optind]);
+				exit(EXIT_FAILURE);
+			}
+			break;
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
 
-FILE *sourceLoad(FILE *fp, char *buffer, int length, int argc, char *argv[])
+void source_load(FILE **fp, char *buffer, int length, int argc, char *argv[])
 {
-	if (fp == NULL)
+	int size; // freadの戻り値を格納
+	if (*fp == NULL)
 	{
 		// 引数がないとき
 		if(argc == 1)
@@ -44,16 +75,20 @@ FILE *sourceLoad(FILE *fp, char *buffer, int length, int argc, char *argv[])
 			exit(EXIT_FAILURE);
 		}
 
-		printf("args: %d\n", argc-optind);
-
 		// file open to read
-		if ((fp = fopen(argv[optind], "r")) == NULL)
+		if ((*fp = fopen(argv[optind], "r")) == NULL)
 		{
 			my_strerror(1, argv[0], argv[optind]);
 			exit(EXIT_FAILURE);
 		}
 	}
-	return fp;
+	size = fread(buffer, sizeof(char), length-1, *fp);
+	if (size  == EOF && !feof(*fp))
+	{
+		my_strerror(3, argv[0], argv[optind]);
+		exit(EXIT_FAILURE);
+	}
+	buffer[size] = '\0';
 }
 
 void my_strerror(short errcode, const char *com, const char *option)
@@ -73,6 +108,12 @@ void my_strerror(short errcode, const char *com, const char *option)
 	{
 		case 1:
 			fprintf(stderr, "no such file or directory \"%s\".\n", option);
+			break;
+		case 2:
+			fprintf(stderr, "failed to close \"%s\".\n", option);
+			break;
+		case 3:
+			fprintf(stderr, "failed to read \"%s\".\n", option);
 			break;
 		// 10~: arguments error
 		case 11:
@@ -99,7 +140,7 @@ void my_strerror(short errcode, const char *com, const char *option)
 
 void usage(const char *com)
 {
-	printf("Usage: %s <source>\n", com);
+	printf("usage: %s <source>\n", com);
 }
 
 void checkopt(int argc, char *argv[])
--- study/prototype.h
+++ study/prototype.h
@@ -1,7 +1,4 @@
-#define DBG(...) (printf("%s %u @%s(): ",__FILE__,__LINE__,__func__), printf(__VA_ARGS__)), puts("")
-#define BUF_LEN 1024
-
-FILE *sourceLoad(FILE *fp, char *buffer, int length, int argc, char *argv[]);
+void source_load(FILE **fp, char *buffer, int length, int argc, char *argv[]);
 void my_strerror(short errcode, const char *com, const char *option);
 void usage(const char *com);
 void checkopt(int argc, char *argv[]);
